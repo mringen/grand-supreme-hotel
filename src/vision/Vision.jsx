b8526d0fdfa360073bc53ecb7c9220fdd41d13f2
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-05-21 22:23:16 +0200

    the first of vision page

--- src/vision/Vision.jsx
+++ src/vision/Vision.jsx
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-
+import './Vision.sass';
+import Hotel_Team from '../shared/picForVision/hotel_team.jpg';
 
 
 class Vision extends Component {
 
     render() {
         return(
-            <div className="visionPage">
+          <div className="visionPage">
+            <div className="titleWrapper">
+              <h1 >Our vision and goal</h1>
             </div>
+                <div className="visonAndGoalTextWrapper">
+               <img className='teamImg' src={Hotel_Team} alt="404"/>
+                   <p className="visonAndGoalText">Bacon ipsum dolor amet meatball sausage cow chicken cupim,
+                    flank venison frankfurter meatloaf. Bacon ipsum dolor amet meatball sausage cow chicken cupim,
+                     flank venison frankfurter meatloaf. </p>
+                  </div>
+
+
+
+          </div>
         )
     }
 }
