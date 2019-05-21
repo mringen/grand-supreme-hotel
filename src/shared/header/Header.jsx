2a549dcb614d69d6156489c69e4bea7bea4a176f
Henrik <henrik.nilsson88@gmail.com>
2019-05-21 18:03:16 +0200

    Header and footer Completed, responsive with Hamburger meny

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -3,10 +3,40 @@ import './Header.sass'
 
 
 class Header extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        activeBurger: false
+      }
+    }
+    toggleNav = () => {
+      const currentState = this.state.activeBurger;
+      this.setState({ activeBurger: !currentState });
+    }
+    linkClicked = () => {
+      console.log('linked clicked')
+    }
 
     render() {
         return(
-            <div className="test">Header</div>
+            <div className="navMainContainer">
+              <h4 className="loggo">Luxury Hotel</h4>
+              <ul className="navLinks">
+                <li>{this.props.landingLink}</li>
+                <li>{this.props.visionLink}</li>
+                <li>{this.props.bookingLink}</li>
+              </ul>
+              <div className="burger" onClick={this.toggleNav}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+              </div>
+              <ul className={this.state.activeBurger ? 'active-burger' : 'not-active-burger'}>
+                <li onClick={this.toggleNav}>{this.props.landingLink}</li>
+                <li onClick={this.toggleNav}>{this.props.visionLink}</li>
+                <li onClick={this.toggleNav}>{this.props.bookingLink}</li>
+              </ul>
+            </div>
         )
     }
 }
