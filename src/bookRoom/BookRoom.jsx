b24cf32cab4ae9238ccdab717209769857baff69
Niclas Hammarström <n.hammarstrom@hotmail.se>
2019-05-21 13:35:18 +0200

    npm calendar

--- src/bookRoom/BookRoom.jsx
+++ src/bookRoom/BookRoom.jsx
@@ -9,44 +9,69 @@ class BookRoom extends Component {
         date: new Date(),
         rooms: ['Queen Room','King Room', 'Supreme Room' ],
         tier: [1, 2, 3, 4],
-        calender: false
+        calendar: true,
+        dateFrom: '',
+        dateTo: ''
     }
+    // let newDate;
 
 
     bookFromDate = () => {
-        console.log('funk bookFromDate');
+        let calendarView;
+
+        if (this.state.date) {
+            calendarView = <Calendar
+            onChange={this.setDate}
+            value={this.state.date}
+            />
+        }
+        else {
+            calendarView = '';
+        }
+        this.setState({calendar: !this.state.calendar})
     }
 
     bookToDate = () => {
         console.log('funk bookToDate');
     }
 
-    onChange = date => this.setState({
-        date
-    })
+    setDateFrom = () => {
+        // this.setState({dateFrom: this.state.date})
+    }
+
+    // setDateTo = () => {}
+
+    setDate = date => {
+        this.setState({ date })
+        this.bookFromDate(date)
+    }
 
     render() {
+        // console.log('date: ', this.state.date);
+        // console.log('datefrom: ', this.state.datefrom);
         let rooms = this.state.rooms.map((room, index) => {return <option key={index}> {room} </option> })
         let tiers = this.state.tier.map((tier, index) => {return <option key={index}> {tier} </option> })
 
         return(
             <div className="bookContainer">
 
-            <select name="rooms">
-                {rooms}
-            </select>
-            
-            <select name="tier">
-                {tiers}
-            </select>
+            <select> {rooms} </select>
 
-            <span onClick={this.bookFromDate}> Date from <input /> </span>
+            <select> {tiers} </select>
+
+            <span onClick={this.bookFromDate}> Date from
+                <input onChange={this.setDateFrom} value={this.state.dateFrom}
+                />
+                {this.calendarView}
+            </span>
+
+
             <span onClick={this.bookToDate}> Date to <input /> </span>
 
-            <Calendar
-                onChange={this.onChange}
+            {/*<Calendar
+                onChange={this.setDate}
                 value={this.state.date}
-            />
+            />*/}
             </div>
         )
     }
