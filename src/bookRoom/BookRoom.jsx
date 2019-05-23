83d153fe759c35f7bfe72bec8f955cf29f53b4d8
Niclas Hammarström <n.hammarstrom@hotmail.se>
2019-05-23 09:53:34 +0200

    date booker

--- src/bookRoom/BookRoom.jsx
+++ src/bookRoom/BookRoom.jsx
@@ -9,7 +9,8 @@ class BookRoom extends Component {
         date: new Date(),
         rooms: ['Queen Room','King Room', 'Supreme Room' ],
         tier: [1, 2, 3, 4],
-        calendar: false,
+        calendarFrom: false,
+        calendarTo: false,
         dateFrom: '',
         dateTo: ''
     }
@@ -19,31 +20,31 @@ class BookRoom extends Component {
     bookFromDate = () => {
 
         this.setState({
-            calendar: !this.state.calendar
+            calendarFrom: !this.state.calendarFrom
         })
     }
 
     bookToDate = () => {
         console.log('funk bookToDate');
         this.setState({
-            calendar: !this.state.calendar
+            calendarTo: !this.state.calendarTo
         })
     }
 
-    setDateFrom = () => {
-        // this.setState({dateFrom: this.state.date})
-    }
+    // setDateFrom = () => {
+    //     // this.setState({dateFrom: this.state.date})
+    // }
 
-    setDateTo = () => {}
+    // setDateTo = () => {}
 
-    setDate = date => {
+    setDateFrom = date => {
         date.setHours(date.getHours()+12);  // kompensera för tidszoner
         console.log('Calendar onchange:', date)
 
         this.setState({ date: date, dateFrom: date.toISOString().substring(0, 10) })
     }
 
-    setDate = date => {
+    setDateTo = date => {
         date.setHours(date.getHours()+12);  // kompensera för tidszoner
         console.log('Calendar onchange:', date)
 
@@ -51,36 +52,49 @@ class BookRoom extends Component {
     }
 
     render() {
-        console.log('calendarView:', this.state.calendarView);
-        console.log('date: ', this.state.date);
         let rooms = this.state.rooms.map((room, index) => {return <option key={index}> {room} </option> })
         let tiers = this.state.tier.map((tier, index) => {return <option key={index}> {tier} </option> })
-        let calendarView = null;
-        if( this.state.calendar ) {
-            calendarView = <Calendar
-                onChange={this.setDate}
+        let calendarCheckInDate = null;
+        let calendarCheckOutDate = null;
+        if( this.state.calendarFrom ) {
+            calendarCheckInDate = <Calendar
+                onChange={this.setDateFrom}
+                value={this.state.date}
+                />
+        }
+        if( this.state.calendarTo ) {
+            calendarCheckOutDate = <Calendar
+                onChange={this.setDateTo}
                 value={this.state.date}
                 />
         }
 
         return(
             <div className="bookContainer">
+                <h1>Choose Room</h1>
 
-            <select> {rooms} </select>
-
-            <select> {tiers} </select>
-
-            <span onClick={this.bookFromDate} className="dateTo"> Date from
-                <input className="dateValue" onChange={this.setDateFrom} value={this.state.dateFrom} />
-
-            </span>
-
-            {calendarView}
+                <div className="container">
+                    <span onClick={this.bookFromDate} className="dateTo"> Date from
+                        <input className="dateValue" onChange={this.setDateFrom} value={this.state.dateFrom} />
+                    </span>
+                    <span className="calendar">{calendarCheckInDate}</span>
+                </div>
 
 
-            <span onClick={this.bookToDate}> Date to
-                <input className="dateValue" onChange={this.setDateTo} value={this.state.dateTo} />
-            </span>
+                <div className="container">
+                    <span onClick={this.bookToDate} className="dateTo"> Date to
+                        <input className="dateValue" onChange={this.setDateTo} value={this.state.dateTo} />
+                    </span>
+                    <span className="calendar">{calendarCheckOutDate}</span>
+                </div>
+
+                <div className="containerRoomPerson">Room
+                    <select> {rooms} </select>
+                </div>
+
+                <div className="containerRoomPerson"> Person
+                    <select> {tiers} </select>
+                </div>
 
             </div>
         )
