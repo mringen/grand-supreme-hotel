f9908363bd4547c593fd423be100f400bb022d8d
Niclas Hammarström <n.hammarstrom@hotmail.se>
2019-05-28 19:06:16 +0200

    fixes with link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
 
 import './App.sass';
 import Header from './shared/header/Header';
@@ -10,39 +8,28 @@ import LandingPage from './landingPage/LandingPage';
 import Vision from './vision/Vision';
 import BookRoom from './bookRoom/BookRoom';
 import RoomMaker from './roomMaker/RoomMaker';
-import Database from './database/Database';
 import ConfirmBooking from './confirmBooking/ConfirmBooking';
-// import LinkToConfirm from './database/LinkToConfirm';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header
-              landingLink={<Link to="/LandingPage/">Landing </Link>}
-              visionLink={<Link to="/Vision/">vision </Link>}
-              bookingLink={<Link to="/BookRoom/">BookRoom </Link>}
-              roomMakerLink={<Link to="/RoomMaker/"> Admin</Link>}
-          />
+    return (
+      <Router>
+        <div className="App">
+            <Header
+                landingLink={<Link to="/LandingPage/">Landing </Link>}
+                visionLink={<Link to="/Vision/">vision </Link>}
+                bookingLink={<Link to="/BookRoom/">BookRoom </Link>}
+                roomMakerLink={<Link to="/RoomMaker/"> Admin</Link>}
+            />
 
-          <Database confirmBookingLink={<Link to="/ConfirmBooking/">här kommer jag</Link>} />
-
-          <Route path="/ConfirmBooking" component={ConfirmBooking}/>
-          <Route path="/LandingPage/"  component={LandingPage} />
-          <Route path="/Vision/"  component={Vision} />
-          <Route path="/BookRoom/"  component={BookRoom} />
-          <Route path="/RoomMaker" component={RoomMaker}/>
-
+            <Route path="/ConfirmBooking" component={ConfirmBooking}/>
+            <Route path="/LandingPage/"  component={LandingPage} />
+            <Route path="/Vision/"  component={Vision} />
+            <Route path="/BookRoom/"  component={BookRoom} />
+            <Route path="/RoomMaker" component={RoomMaker}/>
         <Footer />
-
-
-
-
-
-
-      </div>
+        </div>
     </Router>
-  );
+    );
 }
 
 export default App;
