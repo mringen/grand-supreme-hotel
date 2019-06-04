7953eee359820f09d4ee7248c4dcb70057e7d294
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-06-04 15:59:02 +0200

    nästan klart med sidan

--- src/vision/Vision.jsx
+++ src/vision/Vision.jsx
@@ -1,35 +1,54 @@
 import React, {Component} from 'react';
 import './Vision.sass';
-
+import TeamImg from '../shared/picForVision/hotel_team.jpg'
+import FeelGood from '../shared/picForVision/feelGood.jpg'
 class Vision extends Component {
 
     render() {
         return(
           <div className="visionPage">
-            <div className="titleWrapper">
-              <img className="teamImg" alt="not working" src="https://www.travelmarket.se/gfx/top/hotel_01.jpg"/>
 
+      <div className="titleWrapper">
+
+        <img src={TeamImg} className="OurTeam"/>
+          <h1 className='teamTitle'>Our team </h1>
+            <div className="teamText">
+            <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
+            doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
+            boudin sausage ground round burgdoggen tongue ribeye beef ribs shankle shank chicken beef picanha. Short loin tail biltong, strip steak shank tri-tip t-bone pork rump. </p>
             </div>
-            <div className="visonAndGoalWrapper">
+        </div>
 
-              <p className="ourTeam">Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola.  </p>
-
-                    <h2 className="teamTitle"> Our Team </h2>
-                    <p className="ourTeam">Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
-                    doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
-                     boudin sausage ground round burgdoggen tongue ribeye beef ribs shankle shank chicken beef picanha. Short loin tail biltong, strip steak shank tri-tip t-bone pork rump. </p>
-
-
-                     <div className="row" >
-                     <div>
-
-                     </div>
-                      </div>
-
-
-            </div>
+        <div className="feelGoodWrapper">
+            <div className="feelGood">
+              <h1> You'r experience </h1>
+                <div>
+                  <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
+                  doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
+                  boudin sausage ground round burgdoggen tongue</p>
+                </div>
             </div>
 
+            <div className="mrClean">
+              <h1> You'r experience </h1>
+                <div>
+                  <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
+                  doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
+                  boudin sausage ground round burgdoggen tongue</p>
+                </div>
+            </div>
+
+            <div className="massage">
+              <h1> You'r experience </h1>
+              <div>
+                  <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
+                  doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
+                  boudin sausage ground round burgdoggen tongue</p>
+                </div>
+            </div>
+        </div>
+
+            </div>
         )
     }
 }
