3b2282c9962df5bdf19f2a871c89084d29cc14cc
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-06-05 14:10:50 +0200

    vison page

--- src/vision/Vision.jsx
+++ src/vision/Vision.jsx
@@ -10,7 +10,7 @@ class Vision extends Component {
 
       <div className="titleWrapper">
 
-        <img src={TeamImg} className="OurTeam"/>
+        <img src={TeamImg} alt="404 "className="OurTeam"/>
           <h1 className='teamTitle'>Our team </h1>
             <div className="teamText">
             <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
@@ -30,7 +30,7 @@ class Vision extends Component {
             </div>
 
             <div className="mrClean">
-              <h1> You'r experience </h1>
+              <h1> Clean and freash </h1>
                 <div>
                   <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
                   doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
@@ -39,12 +39,33 @@ class Vision extends Component {
             </div>
 
             <div className="massage">
-              <h1> You'r experience </h1>
+              <h1> Relaxation</h1>
               <div>
                   <p >Bacon ipsum dolor amet frankfurter beef ribs prosciutto pork belly fatback bresaola. Flank boudin ham hock,
                   doner fatback bacon chicken biltong shoulder buffalo capicola strip steak pork chop cupim pastrami. Biltong leberkas hamburger,
                   boudin sausage ground round burgdoggen tongue</p>
                 </div>
+            </div>
+        </div>
+
+        <div>
+            <div className="aboutUs">
+            <div >
+              <img src="https://cdn2-www.comingsoon.net/assets/uploads/2016/11/BlackWhiteLogan-1.jpg" alt="404" className="henrikImg"/>
+              <h2> Henrick Nilsson</h2>
+                <p> AC/CD & Piccolo</p>
+
+              </div>
+                <div>
+              <img src ="https://www.thefashionisto.com/wp-content/uploads/2017/10/Chris-Hemsworth-2017-GQ-Australia-Cover-Photo-Shoot-003-630x350.jpg"  alt="404" />
+                <h2> Nicklas Hammarström</h2>
+                  <p> ADD & House maid </p>
+                  </div>
+                        <div >
+            <img src="http://1.bp.blogspot.com/-aKizAezvB8I/T2HkLVWTsQI/AAAAAAAAaQ8/TpYW3-Lhhik/s1600/bruce-lee.jpg"  alt="404" />
+              <h2> Kamphol Taeng-Iam</h2>
+                <p> ADHD & Yoga master </p>
+                    </div>
             </div>
         </div>
 
